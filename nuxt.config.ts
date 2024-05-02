b8bc3bdefbd7bd73dc06061086d758e581c5c71c
Ant00000ny <Ant00000ny@icloud.com>
2024-05-03 03:58:32 +0800

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  devtools: { enabled: false },
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
 });
